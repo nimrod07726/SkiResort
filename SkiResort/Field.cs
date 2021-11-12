514f59a3a1d76baa8ddb493a4ab2055c8ec81556
dilop <dilop@LAPTOP-T4T2B0MN>
2021-11-12 09:16:20 -0500

    Add project files.

---
+++ SkiResort/Field.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkiResort
+{
+    class Field
+    { 
+        public int[,] Elevations { get; set; }
+        public int[,] Cache { get; set; }
+        public int[] MaximumPath { get; set; }
+        public int MaximumDrop {
+            get
+            {
+                if(MaximumPathLength > 0)
+                {
+                    return MaximumPath.Max() - MaximumPath.Min();
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        public int MaximumPathLength {
+            get
+            {
+                return MaximumPath.Length;
+            }
+        }
+
+        public void PrintMaximumPath()
+        {
+            if(MaximumPathLength > 0)
+            {
+                foreach (int elevation in MaximumPath)
+                {
+                    Console.Write("{0} ", elevation);
+                    Console.WriteLine();
+                }
+            }
+            else
+                Console.WriteLine("empty");
+        }
+
+        public Field()
+        {
+            MaximumPath = new int[] { };
+        }
+
+        public Field(int[,] elevations)
+        {
+            Elevations = elevations;
+            ComputeMaximumPath();
+        }
+
+        private void ComputeMaximumPath()
+        {
+            MaximumPath = new int[] { };
+        }
+
+        
+
+    }
+}
---
+++ SkiResort/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Configuration;
+using System.Linq;
+
+namespace SkiResort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creating elevations 2D-array
+            int[,] elevations = ReadFile();
+            // reading external file
+            if (elevations != null)
+            {
+                // creating Field object
+                Field field = new Field(elevations);
+
+                // retrieving MaximumPathLength
+                Console.WriteLine("MaximumPathLength is {0}", field.MaximumPathLength);
+                // retrieving MaximumDrop
+                Console.WriteLine("MaximumDrop is {0}", field.MaximumDrop);
+                // retrieving MaximumPath
+                Console.Write("MaximumPath is ");
+                field.PrintMaximumPath();
+            }
+            else
+            {
+                Console.WriteLine("No valid elevations field found.");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int[,] ReadFile()
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectPath = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            string fileName;
+            string path;
+
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                fileName = appSettings["FileName"] ?? string.Empty;
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error reading App.config file.");
+                return null;
+            }
+
+            if (fileName != string.Empty)
+            {
+                path = String.Format(@"{0}\input\{1}", projectPath, fileName);
+            }
+            else
+            {
+                Console.WriteLine("Not valid file name found in App.config. Please check the FileName key.");
+                return null;
+            }  
+
+            // parsing input file
+
+            string[] lines = File.ReadAllLines(path);
+            int i = 0, j = 0;
+
+            int[,] elevations = new int[
+                int.Parse(lines[0].Split(' ')[0]),
+                int.Parse(lines[0].Split(' ')[0])
+                ];
+
+            lines = lines.Where((item, index) => index != 0).ToArray();
+
+            foreach (string line in lines)
+            {
+                j = 0;
+                foreach (var elevation in line.Split(' '))
+                {
+                    elevations[i, j] = int.Parse(elevation);
+                    j++;
+                }
+                i++;
+            }
+
+            return elevations;
+        }
+    }
+}
